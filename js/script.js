c601116ecdc98c3b9f69f1dedb71bf55df63300f
Alexander Wunschik <alex@wunschik.net>
2013-04-16 23:01:15 +0200

    fixed the access button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-  var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
-  if (viewportmeta) {
-    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
-    document.body.addEventListener('gesturestart', function() {
-      viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-    }, false);
-  }
+	var viewportmeta = document.querySelectorAll('meta[name="viewport"]')[0];
+	if (viewportmeta) {
+		viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+		document.body.addEventListener('gesturestart', function() {
+			viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+		}, false);
+	}
 }
 
 /*
@@ -28,20 +28,54 @@ $('.column-width').waypoint(function(event, direction) {
 });
 */
 
-/* START GEO Location experiments: */
+
 $(document).ready(function() {
+
+function getUrlParams() {
+	// This function is anonymous, is executed immediately and 
+	// the return value is assigned to QueryString!
+	var query_string = {};
+	var query = window.location.hash.split('#?')[1];
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+			// If first entry with this name
+		if (typeof query_string[pair[0]] === "undefined") {
+			query_string[pair[0]] = pair[1];
+			// If second entry with this name
+		} else if (typeof query_string[pair[0]] === "string") {
+			var arr = [ query_string[pair[0]], pair[1] ];
+			query_string[pair[0]] = arr;
+			// If third or later entry with this name
+		} else {
+			query_string[pair[0]].push(pair[1]);
+		}
+	} 
+	return query_string;
+};
+	
+function createAccessButton() {
+	var urlParams = getUrlParams();
+	console.log("urlParams: ", urlParams);
+	
+	$('#accessBtn').empty().html('<a href="'+urlParams.access+'"><span>Weitersurfen</span></a>');
+}
+createAccessButton();
+
+
+
 
 geo = {};
 geo.position = null;
 geo.isLoaded = false;
 
 function errors_action(error) {
-  switch(error.code) {
-    case error.PERMISSION_DENIED: console.error("Der Nutzer möchte keine Daten teilen."); break;
-    case error.POSITION_UNAVAILABLE: console.error("Die Geodaten sind nicht erreichbar."); break;
-    case error.PERMISSION_DENIED: console.error("Timeout erhalten"); break;
-    default: console.error("Unbekannter Error"); break;
-  }
+	switch(error.code) {
+		case error.PERMISSION_DENIED: console.error("Der Nutzer möchte keine Daten teilen."); break;
+		case error.POSITION_UNAVAILABLE: console.error("Die Geodaten sind nicht erreichbar."); break;
+		case error.PERMISSION_DENIED: console.error("Timeout erhalten"); break;
+		default: console.error("Unbekannter Error"); break;
+	}
 }
 
 function showNearNodes(nodes, position) {	
@@ -54,7 +88,6 @@ function showNearNodes(nodes, position) {
 		node.distance = distance;
 		//console.log("Node " + node.name + ": Distance: " + distance);
 	});
-	
 	
 	nodes.sort(function (a, b) {
 		return ((a.distance < b.distance) ? -1 : ((a.distance > b.distance) ? 1 : 0));
@@ -114,4 +147,3 @@ if (!geo.isLoaded) {
 }
 
 });
-/* END GEO Location experiments: */
